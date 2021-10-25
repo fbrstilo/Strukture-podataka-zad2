8d8575e4f163b17b4219d31a0ca95cc53ef3f637
fbrstilo <fbrstilo@gmail.com>
2021-10-25 19:11:03 +0200

    Finished functions to prepend and append functions

--- source.c
+++ source.c
@@ -11,9 +11,10 @@ typedef struct _person{
 }person;
 
 person* createPerson(char* fname, char* lname);
+person* findEnd(person* headptr);
 void insertAfter(person* prev, person* to_insert);
-void prependList(person* headptr, person to_insert);
-void appendList(person* headptr, person to_insert);
+void prependList(person* headptr, person* to_insert);
+void appendList(person* headptr, person* to_insert);
 void printList();
 void findByLname();
 void deleteElement();
@@ -26,8 +27,14 @@ int main(){
     return 0;
 }
 
-void prependList(person* headptr, person to_insert){
+void prependList(person* headptr, person* to_insert){
+    insertAfter(headptr, to_insert);
+}
 
+void appendList(person* headptr, person* to_insert){
+    person* temp = headptr;
+    temp = findEnd(temp);
+    insertAfter(temp, to_insert);
 }
 
 
@@ -48,6 +55,16 @@ person* createPerson(char* fname, char* lname){
     strcpy(temp->fname, fname);
     strcpy(temp->lname, lname);
     temp->next = NULL;
+
+    return temp;
+}
+
+person* findEnd(person* headptr){
+    person* temp = headptr;
+
+    while(temp->next){
+        temp = temp->next;
+    }
 
     return temp;
 }
